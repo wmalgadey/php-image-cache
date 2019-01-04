f4c5be380c9bda5eead3a5cc09d20ad326a61502
Wolfgang Malgadey <w.malgadey@cp-pro.de>
2019-01-04 13:51:46 +0100

    Update ImageCache.php
    * reformatted
    * throws exceptions instead of exit
    * make image width and height usable from outside

--- src/ImageCache/ImageCache.php
+++ src/ImageCache/ImageCache.php
@@ -17,7 +17,8 @@ namespace ImageCache;
 
 ob_start();
 
-class ImageCache {
+class ImageCache
+{
 
     const memory_value = 128;
 
@@ -65,7 +66,7 @@ class ImageCache {
 
     /**
      * The compression's output quality
-     * @var array 
+     * @var array
      */
     public $quality;
 
@@ -108,34 +109,37 @@ class ImageCache {
      * The extension of the file
      */
     private $cached_filesize;
-    
+
     /**
      * Constructor function
      *
      * @param array $options Options include the keys 'echo' (boolean) and 'cache_time' (integer).  'cache_time' is currently not employed, but in place for future reference.
      * @return object Returns the class object for the user to reference it in the future.
      */
-    public function __construct($options = array()) {
-        if (!$this->can_run_image_cache())
+    public function __construct($options = array())
+    {
+        if (!$this->can_run_image_cache()) {
             $this->error('PHP Image Cache must be run on a server with a bundled GD version.');
+        }
+
         $defaults = array(
             'check_link_cached' => true, // Check link of cached file if is valid by curl
             'echo' => false, // Determines whether the resulting source should be echoed or returned
             'cache_time' => 0, // How long the image should be cached for. If the value is 0, then the cache never expires. Default is 0, never expires.m
-            "quality" => array(// Determines the quality of cache output
+            "quality" => array( // Determines the quality of cache output
                 "jpeg" => 85,
-                "png" => 8
+                "png" => 8,
             ),
             "cached_image_directory" => dirname(__FILE__) . "/php-image-cache",
-            "cached_image_url" => "",
-            "cached_directory_version" => ""
+            "cached_image_url" => "/images/cached",
+            "cached_directory_version" => "",
         );
         $this->options = (object) array_merge($defaults, $options);
         $this->cached_image_directory = $this->options->cached_image_directory;
         $this->cached_image_url = rtrim($this->options->cached_image_url, "/");
         $this->quality = (object) array(
-                    "jpeg" => $this->options->quality["jpeg"],
-                    "png" => $this->options->quality["png"]
+            "jpeg" => $this->options->quality["jpeg"],
+            "png" => $this->options->quality["png"],
         );
         $this->cached_directory_version = $this->options->cached_directory_version;
         $this->check_link_cached = $this->options->check_link_cached;
@@ -148,7 +152,8 @@ class ImageCache {
      *
      * @return bool
      */
-    public function can_run_image_cache() {
+    public function can_run_image_cache()
+    {
         $gd_info = gd_info();
         $this->gd_version = false;
         if (preg_match('#bundled \((.+)\)$#i', $gd_info['GD Version'], $matches)) {
@@ -164,20 +169,28 @@ class ImageCache {
      *
      * @return string The source file to be referenced after compressing an image
      */
-    public function cache($image, $version = "") {
-ob_start();
-        if ( ! is_writable($this->cached_image_directory))
-            $this->error( $this->cached_image_directory . ' must writable!');
+    public function cache($image, $version = "")
+    {
+        ob_start();
+        if (!is_writable($this->cached_image_directory)) {
+            $this->error($this->cached_image_directory . ' must writable!');
+        }
 
-        if (!is_string($image))
+        if (!is_string($image)) {
             $this->error('Image source given must be a string.');
+        }
 
         $this->image_src = $image;
         $this->image_src_version = $version;
-        $this->pre_set_class_vars();
+
+        if (!$this->pre_set_class_vars()) {
+            return $image;
+        }
 
         // If the image hasn't been server up at this point, fetch, compress, cache, and return
         if ($this->cached_file_exists()) {
+			$this->set_image_size();
+
             $this->src_filesize = filesize($this->image_src);
             $this->cached_filesize = filesize($this->cached_filename);
             if ($this->src_filesize < $this->cached_filesize) {
@@ -190,8 +203,10 @@ ob_start();
         if ($this->is_remote) {
             $this->download_image();
         }
-        if (!$this->fetch_image())
+        if (!$this->fetch_image()) {
             $this->error('Could not copy image resource.');
+        }
+
         $this->src_filesize = filesize($this->image_src);
         $this->cached_filesize = filesize($this->cached_filename);
         if ($this->src_filesize < $this->cached_filesize) {
@@ -205,15 +220,17 @@ ob_start();
     /**
      * Downloads a remote file and stores it locally to be used for compression
      */
-    private function download_image() {
-        $image_resource = file_get_contents($this->image_src);
+    private function download_image()
+    {
         $basename = basename($this->image_src);
         if (!stripos($basename, '.' . $this->file_extension)) {
             $basename .= '.' . $this->file_extension;
         }
-        $image_location = dirname($this->cached_image_directory) . '/' . $basename;
+        $image_location = $this->cached_image_directory . '/original/' . $basename;
         if (!file_exists($image_location)) {
-            if (!file_put_contents($image_location, $image_resource)) {
+			$image_resource = file_get_contents($this->image_src);
+
+			if (!file_put_contents($image_location, $image_resource)) {
                 $this->error('Could not download the remote image');
             }
         }
@@ -225,7 +242,8 @@ ob_start();
      *
      * @return bool If the directory was successfully created or not
      */
-    private function make_cache_directory() {
+    private function make_cache_directory()
+    {
         if (is_dir($this->cached_image_directory)) {
             return true;
         }
@@ -238,20 +256,38 @@ ob_start();
         return true;
     }
 
+
+	public $image_width;
+	public $image_height;
+
+	private function set_image_size() {
+		$image_size = getimagesize($this->image_src);
+		$image_width = $image_size[0];
+		$image_height = $image_size[1];
+
+		$this->image_width = $image_width;
+		$this->image_height = $image_height;
+	}
+
     /**
      * Fetch the image as a resource and save it into the cache directory.
      *
      * @source http://stackoverflow.com/questions/9839150/image-compression-in-php
      * @return If the image was successfully created or not
      */
-    private function fetch_image() {
-        $image_size = getimagesize($this->image_src);
-        $image_width = $image_size[0];
-        $image_height = $image_size[1];
+    private function fetch_image()
+    {
+		$this->set_image_size();
+
+		$image_width = $this->image_width;
+		$image_height = $this->image_height;
+
         $file_mime_as_ext = end(@explode('/', $this->file_mime_type));
         $image_dest_func = 'imagecreate';
-        if ($this->gd_version >= 2)
+        if ($this->gd_version >= 2) {
             $image_dest_func = 'imagecreatetruecolor';
+        }
+
         if (in_array($file_mime_as_ext, array('gif', 'jpeg', 'png'))) {
             $image_src_func = 'imagecreatefrom' . $this->file_extension;
             $image_create_func = 'image' . $this->file_extension;
@@ -298,13 +334,14 @@ ob_start();
      * @param string $src The url to check validate
      * @return string The URL of the image
      */
-    private function docroot_to_url($src = null) {
+    private function docroot_to_url($src = null)
+    {
         if (is_null($src)) {
             $src = $this->cached_filename;
         }
         if (empty($this->cached_image_url)) {
             $image_path = str_replace($_SERVER['DOCUMENT_ROOT'], '', $src);
-            $image_url =  $_SERVER['REQUEST_SCHEME']."://" . $_SERVER['HTTP_HOST'].'/' . substr($image_path, 1);
+            $image_url = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'] . '/' . substr($image_path, 1);
         } else {
             $image_url = $this->cached_image_url . "/" . basename($src);
         }
@@ -317,11 +354,20 @@ ob_start();
     /**
      * Sets up all class variables in one central function.
      */
-    private function pre_set_class_vars() {
-        $this->set_file_mime_type();
-        $this->set_cached_filename();
-        $this->set_memory_limit();
-        $this->set_is_remote();
+    private function pre_set_class_vars()
+    {
+        try {
+            $this->set_file_mime_type();
+            $this->set_cached_filename();
+            $this->set_memory_limit();
+            $this->set_is_remote();
+
+            return true;
+        } catch (\Exception $e) {
+			// nothing
+		}
+
+        return false;
     }
 
     /**
@@ -330,9 +376,11 @@ ob_start();
      * @param string $url The url to check validate
      * @return bool Indicates whether or not the link is broken
      */
-    private function link_is_broken($url) {
-        if (!$this->check_link_cached || !function_exists('curl_init'))
+    private function link_is_broken($url)
+    {
+        if (!$this->check_link_cached || !function_exists('curl_init')) {
             return false;
+        }
 
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -353,19 +401,25 @@ ob_start();
      *
      * @return bool Whether or not the image is remote
      */
-    private function is_image_local() {
-        if (file_exists($this->image_src))
+    private function is_image_local()
+    {
+        if (file_exists($this->image_src)) {
             return true;
+        }
+
         $parsed_src = parse_url($this->image_src);
-        if ($_SERVER['HTTP_HOST'] === $parsed_src['host'])
+        if ($_SERVER['HTTP_HOST'] === $parsed_src['host']) {
             return true;
+        }
+
         return false;
     }
 
     /**
      * Determines if the image is local or being referenced from a remote URL
      */
-    private function set_is_remote() {
+    private function set_is_remote()
+    {
         $this->is_remote = !$this->is_image_local();
     }
 
@@ -376,7 +430,8 @@ ob_start();
      * Employing use of the "alt" and "title" tags on the image (which hopefully you're already doing) will negate any potentially negative impacts on SEO.  See
      * this article for more information: http://searchenginewatch.com/article/2120682/Image-Optimization-How-to-Rank-on-Image-Search
      */
-    private function set_cached_filename() {
+    private function set_cached_filename()
+    {
         $pathinfo = pathinfo($this->image_src);
         $this->cached_filename = $this->cached_image_directory . '/' . md5($this->cached_directory_version . basename($this->image_src) . $this->image_src_version) . '.' . $this->file_extension;
     }
@@ -384,22 +439,28 @@ ob_start();
     /**
      * Simply determines if a compressed of the image that's sent is already compressed or not.
      */
-    private function cached_file_exists() {
+    private function cached_file_exists()
+    {
         if ($this->is_remote) {
             $this->download_image();
         }
-        if (file_exists($this->cached_filename))
+        if (file_exists($this->cached_filename)) {
             return true;
+        }
+
         return false;
     }
 
     /**
      * Stores the file's mime type and validates that the file being compressed is indeed an image.
      */
-    private function set_file_mime_type() {
-        $image_type = exif_imagetype($this->image_src);
-        if (!$image_type)
+    private function set_file_mime_type()
+    {
+        $image_type = @exif_imagetype($this->image_src);
+        if (!$image_type) {
             $this->error('The file you supplied isn\'t a valid image.');
+        }
+
         $this->file_mime_type = image_type_to_mime_type($image_type);
         $this->file_extension = image_type_to_extension($image_type, false);
     }
@@ -407,7 +468,8 @@ ob_start();
     /**
      * Stores the original value of the server's memory limit
      */
-    private function set_memory_limit() {
+    private function set_memory_limit()
+    {
         $this->memory_limit = ini_get('memory_limit');
     }
 
@@ -415,14 +477,16 @@ ob_start();
      * Temporarily increases the servers memory limit to 2480 MB to handle building larger images.  Based on initial
      * tests, this seems to be a large enough increase to handle rebuilding jpg images as large as 4300px wide with no pre-compression.
      */
-    private function increase_memory_limit() {
+    private function increase_memory_limit()
+    {
         ini_set('memory_limit', '2480M');
     }
 
     /**
      * Resets the servers memory limit to its original value
      */
-    private function reset_memory_limit() {
+    private function reset_memory_limit()
+    {
         ini_set('memory_limit', $this->memory_limit);
     }
 
@@ -431,10 +495,12 @@ ob_start();
      *
      * @param String $status The message to be passed to the native `exit()` function
      */
-    private function error($status = null) {
-        if (is_null($status))
+    private function error($status = null)
+    {
+        if (is_null($status)) {
             $status = 'Unknown Error:';
-        exit($status);
-    }
+        }
 
+		throw new \Exception($status);
+    }
 }
